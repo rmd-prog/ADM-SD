be434baa9b58d33fd8e7e750c8a62a8c10e5ccad
rmd-prog <pgsd.unida14@gmail.com>
2026-09-09 22:36:26 +0700

    feat: load global popup loading system

--- assets/role-menu.js
+++ assets/role-menu.js
@@ -121,6 +121,7 @@ function boot(){
   installAuthTokenBridge();
   applyRoleMenu();
   installD1SyncButtonFix();
+  loadScript('assets/loading-system.js?v=1','__ADM_LOADING_SYSTEM');
   loadScript('assets/multi-rombel.js?v=1','__ADM_MULTI_ROMBEL_LOADED');
   loadScript('assets/guru-mapel-access.js?v=2','__ADM_GURU_MAPEL_ACCESS_LOADED');
   loadScript('assets/ai-brain-v41.js?v=411','__ADM_AI_V41_LOADED');
